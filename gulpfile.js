55715fef9088997c0aa0fc3648a8d6bc316ecf78
Jason Snider <jason@jasonsnider.com>
2018-10-20 06:22:21 -0500

    Added gulp

---
+++ gulpfile.js
@@ -0,0 +1,75 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var cleanCSS = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
+var merge = require('merge-stream');
+var scss = require('gulp-sass');
+
+gulp.task('default', ['watch']);
+
+gulp.task('build-css', function(){
+  /*
+  var full = gulp.src([
+    'src/scss/main.scss'
+  ])
+  . pipe(scss())
+  . pipe(concat('main.css'))
+  . pipe(gulp.dest('public/dist/css'));
+  */
+
+  var main = gulp.src([
+    'src/scss/main.scss'
+  ])
+  . pipe(scss())
+  . pipe(cleanCSS())
+  . pipe(concat('main.min.css'))
+  . pipe(gulp.dest('public/dist/css'));
+
+  var home = gulp.src([
+  // 'src/scss/main.scss',
+    'src/scss/home-page.scss'
+  ])
+  . pipe(scss())
+  . pipe(cleanCSS())
+  . pipe(concat('home-page.min.css'))
+  . pipe(gulp.dest('public/dist/css'));
+
+  return merge(main, home);
+});
+
+gulp.task('build-js', function() {
+
+  /*
+  var full = gulp.src([
+    'src/js/main.js',
+    'src/js/cover.js',
+  ])
+  .pipe(concat('home-page.js'))
+  .pipe(gulp.dest('public/dist/js'));
+  */
+
+  var home = gulp.src([
+    'src/js/main.js',
+    'src/js/home.js',
+  ])
+  .pipe(concat('home-page.min.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest('public/dist/js'));
+
+  var article = gulp.src([
+    'src/js/main.js',
+    'src/js/article.js',
+  ])
+  .pipe(concat('article.min.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest('public/dist/js'));
+
+  return merge(home, article);
+});
+
+gulp.task('watch', function(){
+  gulp.watch('./src/scss/**/*.scss', ['build-css']);
+  gulp.watch('./src/js/**/*.js', ['build-js']);
+});
