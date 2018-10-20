e2e66dddf87162f6bb9f802ccb3995b18301a1f7
Jason Snider <jason@jasonsnider.com>
2018-10-20 06:21:40 -0500

    Removed debug code

--- public/dist/js/tools/strlen.js
+++ public/dist/js/tools/strlen.js
@@ -1,5 +1,5 @@
 var str = document.getElementById('InputString');
-console.log(str);
+
 str.addEventListener('keyup', function(){
   document.getElementById('StringLength').innerHTML = str.value.length;
   document.getElementById('Results').style.display = 'inline';
